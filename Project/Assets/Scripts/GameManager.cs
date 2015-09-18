1f4a77acee75f73afaf96bb328510f2366102b32
Artem <artem.grimuta@hotmail.com>
2015-09-18 10:32:20 +0300

    Add

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+	public static int currentScore;
+	public static  int highscore;
+
+	public static int currentLevel=0;
+	public static int unlockedLevel;
+
+	public static void CompleteLevel()
+	{
+		if (currentLevel < 3) {
+						currentLevel += 1;
+						Application.LoadLevel (currentLevel);
+				} 
+		else 
+		{
+			print("You win!");
+		}
+		 
+	}
+
+}
---
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	public float  moveSpeed;
+	public GameObject deathParticles;
+
+	private float maxSpeed=5f;
+	private Vector3 input; 
+
+	private Vector3 spawn;
+	void Start () {
+		spawn = transform.position;
+
+	}
+
+	void Update () {
+		input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		if(rigidbody.velocity.magnitude<maxSpeed)
+		{
+			rigidbody.AddRelativeForce(input * moveSpeed);
+		}
+
+		if (transform.position.y < -2) 
+		{
+				Die ();
+		}
+
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+	if(other.transform.tag=="Enemy")
+		{
+			Die ();
+
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+			if(other.transform.tag=="Goal")
+			{
+			GameManager.CompleteLevel();
+			}
+
+		}
+
+	void Die()
+	{
+		Instantiate(deathParticles, transform.position, Quaternion.Euler(270,0,0));
+		transform.position=spawn;
+	}
+}
+ 
